54ce1494ae2010363f24d998d8112f8935fd8e8f
Saif Alam <saifalam8993@gmail.com>
2022-03-18 16:45:27 +0530

    loginpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
